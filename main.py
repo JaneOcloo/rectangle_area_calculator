b867f9ef1f7975e961711d55cef80c23dd3f446a
JaneOcloo <jane.ocloo1@gmail.com>
2025-03-23 14:54:38 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+length= float(input("Enter the length of the room(in metres):"))
+width= float(input("Enter the width of the room (in metres as well):"))
+
+area= length*width
+print("The area of the room is:", area, "square metres")
